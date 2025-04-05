ad0688dc3c6fea147150bbd721a7bd540615da7d
tano (lap) <tanokido854@gmail.com>
2025-04-05 12:47:44 +0700

    a

--- Scripts csdl_congty.sql
+++ Scripts csdl_congty.sql
@@ -334,3 +334,15 @@ from nhanvien
 join thannhan on nhanvien.manv = thannhan.manv
 where (nhanvien.dem = thannhan.tentn or nhanvien.honv = thannhan.tentn or nhanvien.tennv = thannhan.tentn)  and nhanvien.gioitinh = thannhan.gioitinh;
 --Cau 18
+select nhanvien.honv, nhanvien.dem, nhanvien.tennv, thamgia.mada
+from nhanvien
+join thamgia on thamgia.manv = nhanvien.manv
+join duan on thamgia.mada = duan.mada
+order by nhanvien.mapb, nhanvien.honv, nhanvien.dem;
+--Cau 19
+select phongban.tenpb, count(nhanvien.manv) as 'Tong so nhan vien', max(nhanvien.luong) as 'Muc luong cao nhat', min(nhanvien.luong) as 'Muc luong thap nhat', avg(nhanvien.luong) as 'Muc luong trung binh'
+from phongban
+join nhanvien on phongban.mapb = nhanvien.mapb
+where phongban.tenpb = 'Nghien cuu'
+group by phongban.tenpb;
+--Cau 20
