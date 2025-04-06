43c5f01ffe7396e5680558a171f3ae7ef0291deb
tanokido85@gmail.com <tanokido85@gmail.com>
2025-04-06 21:39:08 +0700

    asdadasdadawdada

--- Scripts csdl_congty.sql
+++ Scripts csdl_congty.sql
@@ -334,3 +334,19 @@ from nhanvien
 join thannhan on nhanvien.manv = thannhan.manv
 where (nhanvien.dem = thannhan.tentn or nhanvien.honv = thannhan.tentn or nhanvien.tennv = thannhan.tentn)  and nhanvien.gioitinh = thannhan.gioitinh;
 --Cau 18
+select nhanvien.honv, nhanvien.dem, nhanvien.tennv, nhanvien.mapb, thamgia.mada 
+from nhanvien
+join thamgia on thamgia.manv = nhanvien.manv
+join duan on thamgia.mada = duan.mada
+order by nhanvien.mapb, nhanvien.honv, nhanvien.dem;
+--Cau 19
+select count(nhanvien.manv), max(nhanvien.luong), min(nhanvien.luong), avg(nhanvien.luong)
+from nhanvien
+join phongban on phongban.mapb = nhanvien.mapb
+where phongban.tenpb = 'Nghien cuu';
+--Cau 20
+select phongban.mapb, count(nhanvien.manv)
+from phongban
+join nhanvien on phongban.mapb = nhanvien.mapb
+group by phongban.mapb;
+--Cau 21
