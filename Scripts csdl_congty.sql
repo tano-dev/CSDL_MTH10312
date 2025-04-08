5de7e19ac0d3ee6991e7efee377479790a988b76
tano (lap) <tanokido854@gmail.com>
2025-04-08 09:25:45 +0700

    a

--- Scripts csdl_congty.sql
+++ Scripts csdl_congty.sql
@@ -346,3 +346,10 @@ join nhanvien on phongban.mapb = nhanvien.mapb
 where phongban.tenpb = 'Nghien cuu'
 group by phongban.tenpb;
 --Cau 20
+select phongban.mapb, count(nhanvien.manv) as 'Tong so nhan vien'
+from phongban
+join nhanvien on phongban.mapb = nhanvien.mapb
+group by phongban.mapb;
+--Cau 21
+select phongban.mapb, avg(nhanvien.luong) as 'Muc luong trung binh'	
+
