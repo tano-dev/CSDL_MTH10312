b588657cd3d17d8b4f564a1d935bae79aac8d642
tano (lap) <tanokido854@gmail.com>
2025-04-09 10:29:27 +0700

    a

--- Mssv_test (2).sql
+++ Mssv_test (2).sql
@@ -1,7 +1,13 @@
 ﻿-- Mssv: 23110163
 -- Ho ten: Nguyen Van Phuc Huy
 -- RDBMS: SQL Server
-
+/*
+date = yy-mm-dd
+datepart(loai_thoigian,cot_thoigian)
+CURRENT_TIMESTAMP: get current date and time
+*/
 create database csdl;
 go
 use csdl;
+
+
